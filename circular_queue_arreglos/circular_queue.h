16b6beae6f8ab2354a9827d54e31dd84f2dae4bc
DianaSanC <dianacomipems@gmail.com>
2025-04-03 21:15:31 -0600

    circular_biqueue.c

---
+++ circular_queue_arreglos/circular_queue.h
@@ -0,0 +1,23 @@
+#ifndef __CIRCULAR_QUEUE_H__
+#define __CIRCULAR_QUEUE_H__
+
+#include <stdbool.h>
+
+#define TAM 100
+
+typedef struct {
+    int head;
+    int tail;
+    int len;
+    int data[TAM];
+} ColaCircular;
+
+ColaCircular ColaCircular_Vacia();
+bool ColaCircular_esVacia(ColaCircular C);
+bool ColaCircular_esLlena(ColaCircular C);
+ColaCircular ColaCircular_Enqueue(ColaCircular C, int d);
+ColaCircular ColaCircular_Dequeue(ColaCircular C);
+int ColaCircular_First(ColaCircular C);
+int ColaCircular_Last(ColaCircular C);
+
+#endif
---
+++ circular_queue_arreglos/circular_queuemain.c
@@ -0,0 +1,67 @@
+#include "colacircular.h"
+#include <stdio.h>
+
+ColaCircular ColaCircular_Vacia() {
+    ColaCircular C;
+    C.head = -1;
+    C.tail = -1;
+    C.len = 0;
+    return C;
+}
+
+bool ColaCircular_esVacia(ColaCircular C) {
+    return C.len == 0;
+}
+
+bool ColaCircular_esLlena(ColaCircular C) {
+    return C.len == TAM;
+}
+
+ColaCircular ColaCircular_Enqueue(ColaCircular C, int d) {
+    if (ColaCircular_esLlena(C)) {
+        printf("La cola está llena\n");
+        return C;
+    }
+
+    if (ColaCircular_esVacia(C)) {
+        C.head = 0;
+        C.tail = 0;
+    } else {
+        C.tail = (C.tail + 1) % TAM;
+    }
+
+    C.data[C.tail] = d;
+    C.len++;
+    return C;
+}
+
+ColaCircular ColaCircular_Dequeue(ColaCircular C) {
+    if (ColaCircular_esVacia(C)) {
+        printf("La cola está vacía\n");
+        return C;
+    }
+
+    if (C.len == 1) {
+        return ColaCircular_Vacia();
+    }
+
+    C.head = (C.head + 1) % TAM;
+    C.len--;
+    return C;
+}
+
+int ColaCircular_First(ColaCircular C) {
+    if (ColaCircular_esVacia(C)) {
+        printf("La cola está vacía\n");
+        return -1; // Valor de error
+    }
+    return C.data[C.head];
+}
+
+int ColaCircular_Last(ColaCircular C) {
+    if (ColaCircular_esVacia(C)) {
+        printf("La cola está vacía\n");
+        return -1; // Valor de error
+    }
+    return C.data[C.tail];
+}
